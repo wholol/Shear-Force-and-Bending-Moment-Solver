f9f14c44be8a01e9981a75d83dc194a6e9c73413
wholol <43025133+wholol@users.noreply.github.com>
2019-06-04 14:51:20 +1000

    Update sfbmd.h

--- SFBMD/sfbmd.h
+++ SFBMD/sfbmd.h
@@ -1,80 +1,40 @@
-#pragma once
+#include "pch.h"
 #include <iostream>
-#include <vector>
+#include "sfbmd.h"
 
-using namespace std;
+int main()
+{
+	BendingMoments beam;
+	beam.initbeam();
+	beam.PointLoadSetup();
+	beam.UDLSetup();
+	beam.TDLSetup();
+	beam.SupportSetup();
 
-
-class beamProperties {
-
-protected:
-	double beamlength; int PointLoadNumber; double PointLoadMagnitude; double PointLoadLocation; double SupportLocation;
-	vector <double> PointLoadLocationVect; vector <double> PointLoadMagnitudeVect;		//vectors for point load magnitude and location.
-	double SupportLocationArray[2];
-
-	int UDLnumber; double UDLmagnitude; double UDLbegin; double UDLend;
-	vector <double> UDLlocation_beginVect; vector <double> UDLlocation_endVect; vector <double> UDLmagnitudeVect;
-	
-public:
-	void initbeam();
-	void PointLoadSetup();
-	void UDLSetup();
-	void SupportSetup();
-	int getPointLoadNumber() const;
-	int getUDLNumber() const;
-};
-
-class ComputeSupports : public beamProperties {
-protected:
-	vector <double> LeftSupports; vector <double> RightSupports;		//these vectors are for the superposition vectors for right and left supports.
-	vector <double> UDLLeftSupports; vector <double> UDLRightSupports;
-
-public:
-	void PointLoadSupports();
-	void UDLSupports();
-};
-
-class ShearForces :public ComputeSupports {
-protected:
-	vector <double> PointForceVal;			//stores all point force values, including support reactions.
-	vector <double> PointForceLocal;		//stores all point force locations, including support values.
-	vector <pair <double, double>> PointLoadPair;					//pairs the pint load magnitude and location.
-	vector <pair <double, double>> PointShearFinalPair;					//pairs the final shear force value and location for point load.
-	vector <double> PointShearVect;				//stores the shear forces.
-
-	vector <double> ShearLocationSuperposition;		// this location vector stores the location for shear for each superposition
-	vector <double> PointShearSuperposition;		// this value stores the values for the point load shears for each superposition.
-	vector <double> ShearLocationFinal;				//final plots after superpostion
-	vector <double> PointShearFinal;				//final shear values.
-	vector <double> removeShear;					//remove redundant shear laods by the point load
-	int element = 0;
-	int storeLastLocation;							//stores the last element of the shear lcoation before adding zeros.
+	if (beam.getTDLnumber() != 0) {
+		beam.TDLSupports();
+		beam.computeTDLShear();
+		beam.computeTDLMoments();
+	}
 
 
-	vector <double> UDLForceVal;	//includes all force values, including
-	vector <double> UDLForceLocal;
-	vector <double> UDLSuperpositionLocation;
-	vector <double> UDLSuperpositionShear;
-	vector <double> UDLShearFinal;
-	vector <double> UDLLocationFinal;
+	if (beam.getPointLoadNumber() != 0) {
+		beam.PointLoadSupports();
+		beam.PointLoadShearSetup();
+		beam.ComputePointShear();
+		beam.ComputePointLoadMoments();
+	}
 
-public:
-	void PointLoadShearSetup();
-	void ComputePointShear();
-	void computeUDLShear();
-	void ParseShearData();			//parses data to excel
-};
+	if (beam.getUDLNumber() != 0) {
+		beam.UDLSupports();
+		beam.computeUDLShear();
+		beam.computeUDLMoments();
+	}
 
-class BendingMoments :public ShearForces {
-protected:
-	vector <double> PointLoadSuperPositionMoments;
-	vector <double> PointMomentsFinal;
+	if (beam.getUDLNumber() != 0 || beam.getPointLoadNumber() != 0 || beam.getTDLnumber() != 0) {
+		beam.ParseShearData();
+		beam.ParseMomentsData();
+	}
+	system("pause");
+}
 
-	vector <double> UDLSuperPositionMoments;
-	vector <double> UDLMomentsFinal;
-
-public:
-	void ComputePointLoadMoments();
-	void computeUDLMoments();
-	void ParseMomentsData();
-};
